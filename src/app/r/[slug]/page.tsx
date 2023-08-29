b9e1a42d1dbc4749dbc2154b31353a93ec952b5f
Shivangam Soni <shivangamsoni99@gmail.com>
2023-08-29 15:52:29 +0530

    Add Subreddit Page Basic UI

---
+++ src/app/r/[slug]/page.tsx
@@ -0,0 +1,48 @@
+import { notFound } from "next/navigation";
+
+import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config";
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import MiniCreatePost from "@/components/MiniCreatePost";
+
+interface PageProps {
+    params: {
+        slug: string;
+    };
+}
+
+export default async function SubredditSlugPage({
+    params: { slug },
+}: PageProps) {
+    const session = await getAuthSession();
+
+    const subreddit = await db.subreddit.findFirst({
+        where: { name: slug },
+        include: {
+            posts: {
+                include: {
+                    author: true,
+                    votes: true,
+                    comments: true,
+                    subreddit: true,
+                },
+            },
+        },
+        take: INFINITE_SCROLLING_PAGINATION_RESULTS,
+    });
+
+    if (!subreddit) {
+        return notFound();
+    }
+
+    return (
+        <>
+            <h1 className="font-bold text-3xl md:text-4xl h-14">
+                r/{subreddit.name}
+            </h1>
+
+            <MiniCreatePost session={session} />
+            {/* TODO: Show Posts in Feed */}
+        </>
+    );
+}
---
+++ src/components/MiniCreatePost.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import type { Session } from "next-auth";
+import { usePathname, useRouter } from "next/navigation";
+import UserAvatar from "./UserAvatar";
+import { Input } from "./ui/Input";
+import { Button } from "./ui/Button";
+import { ImageIcon, Link2 } from "lucide-react";
+
+interface Props {
+    session: Session | null;
+}
+
+export default function MiniCreatePost({ session }: Props) {
+    const router = useRouter();
+    const pathname = usePathname();
+
+    return (
+        <li className="overflow-hidden rounded-md bg-white shadow">
+            <div className="h-full px-6 py-4 flex flex-col sm:flex-row justify-between items-center gap-6">
+                <div className="relative">
+                    <UserAvatar
+                        user={{
+                            name: session?.user.name || null,
+                            image: session?.user.image || null,
+                        }}
+                    />
+
+                    <span className="absolute bottom-0 right-0 w-3 h-3 rounded-full bg-emerald-500 outline outline-2 outline-white" />
+                </div>
+
+                <div className="w-full flex-1 flex flex-col sm:flex-row justify-between items-center gap-1 sm:gap-6">
+                    <Input
+                        readOnly
+                        placeholder="Create Post"
+                        onClick={() => router.push(pathname + "/submit")}
+                    />
+
+                    <div className="flex justify-between items-center gap-1 sm:gap-6">
+                        <Button
+                            variant="ghost"
+                            size="xs"
+                            onClick={() => router.push(pathname + "/submit")}
+                        >
+                            <ImageIcon className="text-zinc-600" />
+                        </Button>
+
+                        <Button
+                            variant="ghost"
+                            size="xs"
+                            onClick={() => router.push(pathname + "/submit")}
+                        >
+                            <Link2 className="text-zinc-600" />
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </li>
+    );
+}
