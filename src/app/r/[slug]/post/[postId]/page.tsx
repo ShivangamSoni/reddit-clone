19cc3585d5ce2c037462b23975965177f2d7f044
Shivangam Soni <shivangamsoni99@gmail.com>
2023-09-09 17:39:48 +0530

    Add Comments UI

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -1,3 +1,4 @@
+import CommentsSection from "@/components/CommentsSection";
 import EditorOutput from "@/components/EditorOutput";
 import PostVoteServer from "@/components/PostVote/Server";
 import PostVoteShell from "@/components/PostVote/Shell";
@@ -60,27 +61,37 @@ export default async function page({
                 </Suspense>
 
                 <div className="sm:w-0 w-full flex-1 bg-white p-4 rounded-sm">
-                    <p className="max-h-40 mt-1 truncate text-xs text-gray-500 divide-x space-x-2">
-                        <span>
-                            Posted by u/
-                            {post?.author.username ?? cachedPost.authorUserName}
-                        </span>
-                        <span className="inline-block pl-2">
-                            {formatTimeToNow(
-                                new Date(
-                                    post?.createdAt ?? cachedPost.createdAt
-                                )
-                            )}
-                        </span>
-                    </p>
-                    <h1 className="text-xl font-semibold py-2 leading-6 text-gray-900">
-                        {post?.title ?? cachedPost.title}
-                    </h1>
+                    <div>
+                        <p className="max-h-40 mt-1 truncate text-xs text-gray-500 divide-x space-x-2">
+                            <span>
+                                Posted by u/
+                                {post?.author.username ??
+                                    cachedPost.authorUserName}
+                            </span>
+                            <span className="inline-block pl-2">
+                                {formatTimeToNow(
+                                    new Date(
+                                        post?.createdAt ?? cachedPost.createdAt
+                                    )
+                                )}
+                            </span>
+                        </p>
+                        <h1 className="text-xl font-semibold py-2 leading-6 text-gray-900">
+                            {post?.title ?? cachedPost.title}
+                        </h1>
+                    </div>
                     <EditorOutput
                         content={post?.content ?? cachedPost.content}
                     />
-                    {/* TODO: Comments */}
-                    Comments
+
+                    <Suspense
+                        fallback={
+                            <Loader2 className="h-5 w-5 animate-spin text-zinc-500" />
+                        }
+                    >
+                        {/* @ts-expect-error Server Component */}
+                        <CommentsSection postId={post?.id ?? cachedPost.id} />
+                    </Suspense>
                 </div>
             </div>
         </div>
